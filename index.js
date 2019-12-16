5d08557e9eded559e63b4d018eb332c1cb0c6cde
Asher Sun <asher_sun@163.com>
2019-12-16 14:17:11 +0800

    feat: 增加创建文件夹、删除文件夹/文件 能力

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-var exec = require('child_process').exec;
-exec('icon-cli xxxxx', function(error, stdout, stderr) {
-  // 获取命令执行的输
-  console.log(error, `stdout ${stderr}==`, stderr);
-});
+const fs = require('fs');
+const path = './test/testFont';
+function mkdir() {
+  fs.stat(path, (err, stats) => {
+    if (err) {
+      console.error('fs.stat---err', err);
+      fs.mkdir(path, {recursive: true}, (mkErr) => {
+        if (mkErr) {
+          console.error('fs.mkdir---mkErr', mkErr);
+          return;
+        }
+        mkdir();
+      })
+      return;
+    }
+    console.log('stats', stats.isDirectory());
+  });
+}
+
+mkdir();
--- utils/tool.js
+++ utils/tool.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
 function checkFileType(fileType) {
   return ['.css', '.js'].includes(fileType)
 }
@@ -27,9 +30,45 @@ function checkHttps(url) {
   return url;
 }
 
+function checkFile(path) {
+  return fsPromises.access(path, fs.constants.R_OK | fs.constants.W_OK);
+}
+
+function rmFile(path) {
+  return fsPromises.unlink(path);
+}
+
+function to(promise) {
+  return new Promise((resolve) => {
+    promise.then((data) => resolve([void 0, data || true])).catch((err) => resolve([err|| true, void 0]));
+  });
+}
+
+function mkDir(path) {
+  return new Promise((resolve, reject) => {
+    fs.stat(path, (err, stats) => {
+      if (err) {
+        fs.mkdir(path, {recursive: true}, (err) => {
+          if (err) {
+            reject();
+            return;
+          }
+          resolve();
+        })
+        return;
+      }
+      stats.isDirectory() && resolve();
+    });
+  })
+}
+
 module.exports = {
   checkFileType,
   getFileType,
   allRight,
   checkHttps,
+  checkFile,
+  rmFile,
+  to,
+  mkDir,
 }
